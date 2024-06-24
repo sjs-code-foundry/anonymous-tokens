2ccfcb88943504a6ac6ce5f7223edb504a34ef0a
sjs-code-foundry <bds.stephen@gmail.com>
2024-06-24 09:14:55 +0100

    Insert date into greeting in preparation for implementing a time readout.

--- app/src/main/java/com/example/anonymoustokens/MainActivity.kt
+++ app/src/main/java/com/example/anonymoustokens/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.anonymoustokens
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.anonymoustokens.ui.theme.AnonymousTokensTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +25,7 @@ class MainActivity : ComponentActivity() {
             AnonymousTokensTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        date = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,9 +35,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(date: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "You have been clean for: $date!",
         modifier = modifier
     )
 }
@@ -42,6 +46,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AnonymousTokensTheme {
-        Greeting("Android")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Greeting(FormatDate("2024-06-01").toString())
+        }
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun FormatDate(date: String): LocalDate? {
+    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+    val formattedDate = LocalDate.parse(date, formatter)
+
+    println(formattedDate)
+    return formattedDate
 }
